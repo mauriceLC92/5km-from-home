8cb7d306be28e261f2207fd834a9065788752565
Maurice <mauricelecordier@gmail.com>
2020-05-01 18:04:59 +0200

    just ship

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Map } from './components/Map'
 
 function App() {
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  function displayLocationInfo(position: any) {
+    const lng = position.coords.longitude;
+    const lat = position.coords.latitude;
+    setLatitude(lat)
+    setLongitude(lng)
+  }
+
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      displayLocationInfo,
+    );
+  }
+  else {
+    console.log('geolocation is not enabled on this browser')
+  }
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
+        <div className='bg-gray-300 flex justify-center flex-col items-center'>
+          <div>
+            <h1 className='text-6xl'>
+              5km From Home
+            </h1>
+          </div>
+          <div>
+          <p className="text-2xl leading-tight tracking-wide">
+            Search for your location below
+          </p>
+          </div>
+        </div>
       </header>
 
-      <main>
-      <Map lat={-33.911321} lng={18.417066} zoom={17}></Map>
+      <main className='flex justify-center'>
+        <div style={{ height: '70vh', width: '70%' }}>
+          {
+            latitude && longitude ?
+            <Map zoom={12} lng={longitude} lat={latitude}/>
+            : <div>Loading....</div>
+          }
+        </div>
       </main>
     </div>
   );
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react'
 require('dotenv').config()
-
 interface MapProps {
   lat: number,
   lng: number,
@@ -9,43 +8,59 @@ interface MapProps {
 }
 
 export const Map: React.FunctionComponent<MapProps> = ({ lat, lng, zoom }) => {
+  const [draggable, ] = useState(true);
+  const [latitude, setLatitude] = useState(lat);
+  const [longitude, setLongitude] = useState(lng);
 
-  const renderMarkers = (map: any, maps: any) => {
-    let marker = new maps.Marker({
-      position: { lat, lng },
+  const renderMarker = (map: any, maps: any) => {
+    const marker = new maps.Marker({
+      position: { lat: latitude, lng: longitude },
       map,
-      title: 'Bertha Centre'
+      title: 'Your Home',
+      draggable,
     });
+
     return marker;
   };
+
+  const renderRadius = (map: any, maps: any) => {
+    const circle = new maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.3,
+      map,
+      center: { lat: latitude, lng: longitude },
+      radius: 4000,
+    });
+    return circle
+  }
+
   return (
-    <div style={{ height: '70vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{
-          key: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
-          language: 'en'
-        }}
-        defaultCenter={{
-          lat,
-          lng
-        }}
-        defaultZoom={zoom}
-        onGoogleApiLoaded={({ map, maps }) => {
-          return (
-            new maps.Circle({
-              strokeColor: '#FF0000',
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: '#FF0000',
-              fillOpacity: 0.3,
-              map,
-              center: {lat, lng},
-              radius: 275,
-            }),
-            renderMarkers(map, maps)
-          )
-        }}
-      />
-    </div>
+    <GoogleMapReact
+      bootstrapURLKeys={{
+        key: `${process.env.REACT_APP_GOOGLE_API_KEY}`,
+        language: 'en'
+      }}
+      defaultCenter={{
+        lat: latitude,
+        lng: longitude
+      }}
+      yesIWantToUseGoogleMapApiInternals
+      draggable={draggable}
+      defaultZoom={zoom}
+      onGoogleApiLoaded={({ map, maps }) => {
+        console.log(maps)
+        return (
+          renderRadius(map, maps),
+          renderMarker(map, maps)
+        )
+      }}
+      onClick={(value) => {
+        setLatitude(value.lat)
+        setLongitude(value.lng)
+      }}
+    />
   )
 }
